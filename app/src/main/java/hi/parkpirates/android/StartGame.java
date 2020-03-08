c2172254941b33387951e72239505a958cd18964
GudrunStella <32542555+GudrunStella@users.noreply.github.com>
2020-03-08 14:05:46 +0000

    Add files via upload

---
+++ app/src/main/java/hi/parkpirates/android/StartGame.java
@@ -0,0 +1,15 @@
+package is.hi.hbv601g.parkpirates;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartGame extends Activity {
+
+    GameView gameView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        gameView = new GameView(this);
+        setContentView(gameView);
+    }
+}
