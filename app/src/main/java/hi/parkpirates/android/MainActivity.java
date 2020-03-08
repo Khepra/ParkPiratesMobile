1d809119b0ecc6e78c996753fdbb9ce24fe6c75b
GudrunStella <32542555+GudrunStella@users.noreply.github.com>
2020-03-08 14:13:27 +0000

    Update MainActivity.java

--- app/src/main/java/hi/parkpirates/android/MainActivity.java
+++ app/src/main/java/hi/parkpirates/android/MainActivity.java
@@ -10,4 +10,11 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	
+	public void startGame(View v) {
+        	Log.i("ImageButton", "clicked");
+        	Intent intent = new Intent(this, StartGame.class);
+        	startActivity(intent);
+        	finish();
+    }
 }
